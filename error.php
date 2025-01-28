f4357bab008c799725a4633825e9ffc0aa912c74
Adam <thomasadam83@hotmail.com>
2025-01-28 10:19:37 -0500

    Added env file

---
+++ error.php
@@ -0,0 +1,40 @@
+<?php
+
+$env = file(__DIR__.'/.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+
+foreach($env as $value)
+{
+  $value = explode('=', $value);  
+  define($value[0], $value[1]);
+}
+
+echo RAPIDAPI_KEY;
+
+die();
+
+$curl = curl_init();
+
+curl_setopt_array($curl, [
+	CURLOPT_URL => "https://fake-random-ip.p.rapidapi.com/",
+	CURLOPT_RETURNTRANSFER => true,
+	CURLOPT_ENCODING => "",
+	CURLOPT_MAXREDIRS => 10,
+	CURLOPT_TIMEOUT => 30,
+	CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+	CURLOPT_CUSTOMREQUEST => "GET",
+	CURLOPT_HTTPHEADER => [
+		"x-rapidapi-host: fake-random-ip.p.rapidapi.com",
+		"x-rapidapi-key: ".RAPIDAPI_KEY
+	],
+]);
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+	echo "cURL Error #:" . $err;
+} else {
+	echo $response;
+}
